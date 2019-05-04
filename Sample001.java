abbd4076711361f674af1fd36c1268ea844c8bc4
Yusa <24yusa@gmail.com>
2019-05-04 23:40:26 +0900

    First

---
+++ Sample001.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+
+/**
+ * @author Yusa
+ *
+ */
+public class Sample001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 0 ; i < 5 ; i++)
+		{
+			System.out.println("Hellooo!!!");
+		}
+	}
+}
